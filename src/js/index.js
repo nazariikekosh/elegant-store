43bc4628e0e16eafdbe2412af3f645584943e9ce
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2024-12-04 06:16:56 +0200

    add slider

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,15 @@
-import mobileNav from './modules/mobile-nav.js';
-mobileNav();
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  slidesPerView: 'auto',
+  spaceBetween: 24,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  }
+});
 
 import tagline from './modules/tagline.js';
 tagline();
