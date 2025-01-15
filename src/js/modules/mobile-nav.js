3affb191b2a6ebbab0b580b4d3cf2f20f0439c09
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2025-01-15 04:25:45 +0200

    clouse mobile-nav

--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -1,13 +1,33 @@
 function mobileNav() {
 	// Mobile nav button
-	const navBtn = document.querySelector('#mobile-nav-btn');
+	const navBtnOpen = document.querySelector('#mobile-nav-btn');
 	const nav = document.querySelector('.mobile-nav');
+	const navBtnClose = document.querySelector('#close-mobile-nav');
+	const mobileNavFade = document.querySelector('.mobile-nav-fade');
 
 
-	navBtn.onclick = function () {
-		nav.classList.toggle('mobile-nav--open');
+	navBtnOpen.onclick = function () {
+		nav.classList.add('mobile-nav--open');
+		mobileNavFade.classList.add('mobile-nav-fade--open');
 		document.body.classList.toggle('no-scroll');
 	};
+
+	navBtnClose.onclick = function () {
+		nav.classList.remove('mobile-nav--open');
+		mobileNavFade.classList.remove('mobile-nav-fade--open');
+		document.body.classList.toggle('no-scroll');
+	};
+
+	nav.addEventListener('click', function (e) {
+		e.stopPropagation();
+	});
+
+	mobileNavFade.onclick = function () {
+		nav.classList.remove('mobile-nav--open');
+		mobileNavFade.classList.remove('mobile-nav-fade--open');
+		document.body.classList.toggle('no-scroll');
+	};
+
 }
 
 export default mobileNav;
