5b601e391f639c3dc8e3ad533d53aa663c2229b1
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2025-01-17 02:28:17 +0200

    add hover

--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -4,30 +4,38 @@ function mobileNav() {
 	const nav = document.querySelector('.mobile-nav');
 	const navBtnClose = document.querySelector('#close-mobile-nav');
 	const mobileNavFade = document.querySelector('.mobile-nav-fade');
+	const mobileNavLinks = document.querySelectorAll('.mobile-nav__link'); // Всі посилання в мобільному меню
 
-
+	// Відкриття меню
 	navBtnOpen.onclick = function () {
 		nav.classList.add('mobile-nav--open');
 		mobileNavFade.classList.add('mobile-nav-fade--open');
-		document.body.classList.toggle('no-scroll');
+		document.body.classList.add('no-scroll');
 	};
 
+	// Закриття меню через кнопку
 	navBtnClose.onclick = function () {
-		nav.classList.remove('mobile-nav--open');
-		mobileNavFade.classList.remove('mobile-nav-fade--open');
-		document.body.classList.toggle('no-scroll');
+		closeMobileNav();
 	};
 
-	nav.addEventListener('click', function (e) {
-		e.stopPropagation();
+	// Закриття меню через натискання на fade
+	mobileNavFade.onclick = function () {
+		closeMobileNav();
+	};
+
+	// Закриття меню при кліку на посилання
+	mobileNavLinks.forEach((link) => {
+		link.onclick = function () {
+			closeMobileNav();
+		};
 	});
 
-	mobileNavFade.onclick = function () {
+	// Функція для закриття мобільного меню
+	function closeMobileNav() {
 		nav.classList.remove('mobile-nav--open');
 		mobileNavFade.classList.remove('mobile-nav-fade--open');
-		document.body.classList.toggle('no-scroll');
-	};
-
+		document.body.classList.remove('no-scroll');
+	}
 }
 
 export default mobileNav;
